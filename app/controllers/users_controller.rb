5fbc528d29b88625305c22e79c7b5ddb0e72698c
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-08-27 14:04:04 +0200

    added chartkick gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,16 @@ class UsersController < ApplicationController
 
   def dashboard
     @challenges = @user.challenges
+    @timeline = [["Washington", "1789-04-29", "1797-03-03"],
+  ["Adams", "1797-03-03", "1801-03-03"],
+  ["Jefferson", "1801-03-03", "1809-03-03"]]
   end
 
   def show
     @challenges = @user.challenges
+    @timeline =   [["Washington", "1789-04-29", "1797-03-03"],
+  ["Adams", "1797-03-03", "1801-03-03"],
+  ["Jefferson", "1801-03-03", "1809-03-03"]]
   end
 
   def edit
